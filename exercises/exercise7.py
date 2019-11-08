3cf10aafd1a746f674813db9771b75f1d60db6b2
Roelle Goze <contactlhen@gmail.com>
2019-11-08 21:33:24 +0800

    Exercise 7

--- exercises/exercise7.py
+++ exercises/exercise7.py
@@ -7,7 +7,17 @@
 
 class Car:
 
-    def __init__(self,year, make, model):
-        self.year = year
-        self.make = make
+    def __init__(self, model="", year=0, maker=""):
         self.model = model
+        self.year = year
+        self.maker = maker
+
+    def age(self):
+    	x = 2019 - self.year
+    	return x
+
+wigo = Car("Wigo", 2014, "Toyota")
+print(wigo.age())
+
+fortuner = Car("Fortuner", 2011, "Toyota")
+print(fortuner.age())
